632f1dfbafec130984295ec059fe79fdf21e626a
Kim Ravn Hansen <krh@krh-workstation.local>
2017-03-28 07:30:44 +0200

    Prune all. Fresh start.

--- app/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$app->get('/', function () use ($app) {
-    return view('welcome');
-});
--- app/yml2json.php
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env php
-<?php
-
-echo json_encode(
-    yaml_parse(file_get_contents('php://stdin')),
-    JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
-);
