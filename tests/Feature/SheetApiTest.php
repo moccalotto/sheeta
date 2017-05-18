0738b087f5d5f756bdef90572805b7afe665df7f
Kim Ravn Hansen <krh@krh-workstation.internal>
2017-05-18 13:53:30 +0200

    Refactor tests.

--- tests/Feature/SheetApiTest.php
+++ tests/Feature/SheetApiTest.php
@@ -11,12 +11,19 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 /**
  * Test the SheetsController
+ *
+ * @codingStandardsIgnoreStart
  */
 class SheetApiTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function testMustHaveApiToken()
+    /**
+     * Ensure that we get a sane error if we don't authenticate.
+     *
+     * @test
+     */
+    public function give_sane_response_if_user_not_authenticated()
     {
         $response = $this->json('POST', 'api/sheets', []);
 
@@ -25,8 +32,10 @@ class SheetApiTest extends TestCase
 
     /**
      * Ensure that the response code is correct if an entity cannot be found.
+     *
+     * @test
      */
-    public function testSaneErrorIfSheetNotFound()
+    public function give_sane_response_if_sheet_could_not_be_found()
     {
         $user = factory(User::class)->create();
 
@@ -60,8 +69,10 @@ class SheetApiTest extends TestCase
      * we do not want people to be able to
      * create sheets belonging to other users,
      * or otherwise corrupt the DB.
+     *
+     * @test
      */
-    public function testCreation()
+    public function it_can_create_a_new_sheet()
     {
         $user = factory(User::class)->create();
 
@@ -85,8 +96,10 @@ class SheetApiTest extends TestCase
 
     /**
      * Test the fetching of a single sheet.
+     *
+     * @test
      */
-    public function testFetch()
+    public function it_can_return_a_single_sheet()
     {
         $user = factory(User::class)->create();
 
@@ -113,8 +126,10 @@ class SheetApiTest extends TestCase
      * we do not want people to be able to
      * create sheets belonging to other users,
      * or otherwise corrupt the DB.
+     *
+     * @test
      */
-    public function testClone()
+    public function it_can_clone_a_single_sheet()
     {
         $user = factory(User::class)->create();
 
--- tests/Unit/SheetClassTest.php
+++ tests/Unit/SheetClassTest.php
@@ -2,14 +2,36 @@
 
 namespace Tests\Unit;
 
+use App\User;
+use App\Sheet;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+/**
+ * Assertions about the App\Sheet class
+ *
+ * @codingStandardsIgnoreStart
+ */
 class SheetClassTest extends TestCase
 {
-    public function testTrue()
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function it_can_be_cloned()
     {
-        $this->assertTrue(true);
+        $sheet = factory(Sheet::class)->create();
+
+        $newUser = factory(User::class)->create();
+
+        $clone = $sheet->createClone($newUser);
+
+        $this->assertNotEquals($sheet->id, $clone->id);
+        $this->assertEquals($newUser->id, $clone->user_id);
+        $this->assertEquals($sheet->id, $clone->original_id);
+        $this->assertEquals($sheet->clone_level + 1, $clone->clone_level);
+        $this->assertEquals(1, $clone->version);
     }
 }
